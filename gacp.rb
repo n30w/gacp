6412cc3f02a221b6f551cb80a322cd5cbb45fbc6
n30w <rocketneo@gmail.com>
2022-09-29 20:28:12 -1000

    Added colorizer, added dependencies

--- gacp.rb
+++ gacp.rb
@@ -1,17 +1,43 @@
 #!/usr/bin/env ruby
 
-add = "git add \."
-push = "git push"
+require 'rubygems'
+require 'bundler/setup'
+require 'colorized_string'
 
-system(add)
+ColorizedString.colors                       # return array of all possible colors names
+ColorizedString.modes                        # return array of all possible modes
+ColorizedString.disable_colorization = false
 
-puts "Added files to Git for staging..."
-puts "Add a commit message:"
 
-msg = gets.chomp
-until msg
-  puts "Please input a valid commit message:"
+# Returns stdout on success, false on failure, nil on error
+# Nabbed from https://bit.ly/3fy0YEh
+def syscall(*cmd)
+  begin
+    stdout, stderr, status = Open3.capture3(*cmd)
+    status.success? && stdout.slice!(0..-(1 + $/.size)) # strip trailing eol
+  rescue
+  end
 end
 
-system("git commit -m \"#{msg}\"")
-system(push)
+
+# TODO:
+# Add more options for gacp
+
+expect = ColorizedString.new("> ").yellow
+
+system "git add \."
+puts "✅ Added files to Git for staging"
+
+puts "📝 Commit message:"
+print expect
+msg = gets.chomp
+until msg.length > 1 do
+  puts "🤨 " +  ColorizedString.new("Please input a valid commit message:").red
+  print expect
+end
+
+system "git commit -m \"#{msg}\""
+puts "💍 Committed 💒"
+system "git push"
+puts "📍 " + ColorizedString.new("Success").light_green + "! Pushed to Github 🤓"
+
